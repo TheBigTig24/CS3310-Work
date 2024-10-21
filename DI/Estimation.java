ec8b2864a3ca04bd91add0629b2b11a9c161e20a
Caleb C <kaichun24@gmail.com>
2024-10-21 14:38:59 -0700

    add new folder

---
+++ DI/Estimation.java
@@ -0,0 +1,19 @@
+package DI;
+
+import java.util.Scanner;
+
+public class Estimation {
+    
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        
+    }
+
+    public static float integrate() {
+        return 69;
+    }
+
+    public static float derivate() {
+        return 69;
+    }
+}
